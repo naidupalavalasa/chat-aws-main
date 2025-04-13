7b1654bb1f70bd41a5a40c0f8b6cbf12a4ab716d
naidupalavalasa <naidupalavalasa17@gmail.com>
2025-04-13 12:06:00 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <div className='navbar'>
-      <span className="logo"> <marque>Chat Me</marque></span>
+      <span className="logo"> <marque>Chat Box</marque></span>
       <div className="user">
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
